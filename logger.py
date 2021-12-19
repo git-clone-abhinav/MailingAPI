71fb85d3c7173fa41ce8c1e555cbe17fc97e0a1e
git-clone-abhinav <abhi.anu003@gmail.com>
2021-12-19 20:06:55 +0530

    discord webhook

--- logger.py
+++ logger.py
@@ -1,10 +1,16 @@
 import time
 import os
 from dotenv import load_dotenv
-
+from discord import Webhook, RequestsWebhookAdapter
+webhook = Webhook.from_url("https://discord.com/api/webhooks/922134536605814826/_JWxho2p4cQFE54yWfFMmJwh57kpSo7kgWH5Z3RcznVhYd2JSyQm7cXhprNgfmhMFjvO", adapter=RequestsWebhookAdapter())
 load_dotenv()
 logPath = os.getenv('logs')
 def log_it(message):
+    webhook.send(message)
     with open(logPath, 'a') as f:
         f.write(f"{time.ctime()} - {message}\n".format())
         f.close()
+
+
+
+
